c47fc9107129203e77655f8e815bba3028e251bc
JakubArtysiewicz <jakub.artysiewicz@infotech.edu.pl>
2026-02-18 17:28:08 +0100

    Zrobione pod egzamin

--- egzamin2_desktop/MainWindow.xaml.cs
+++ egzamin2_desktop/MainWindow.xaml.cs
@@ -22,10 +22,10 @@ namespace egzamin2_desktop
             this.Rslider.Value = 255;
             this.Gslider.Value = 255;
             this.Bslider.Value = 255;
-        }
-        private void onSliderChange(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            RGBBackgroundColorChange();
+            this.Rlabel.Content = 255;
+            this.Glabel.Content = 255;
+            this.Blabel.Content = 255;
+            this.Pobrane.Content = "255,255,255";
         }
         private void RGBBackgroundColorChange()
         {
@@ -33,6 +33,36 @@ namespace egzamin2_desktop
             byte GsliderValue = (byte)this.Gslider.Value;
             byte BsliderValue = (byte)this.Bslider.Value;
             RGBlabel.Background = new SolidColorBrush(Color.FromRgb(RsliderValue, GsliderValue, BsliderValue));
+        }
+
+        private void onRSliderChange(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte RsliderValue = (byte)this.Rslider.Value;
+            RGBBackgroundColorChange();
+            this.Rlabel.Content = RsliderValue.ToString();        
+        }
+
+        private void onGSliderChange(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte GsliderValue = (byte)this.Gslider.Value;
+            RGBBackgroundColorChange();
+            this.Glabel.Content = GsliderValue.ToString();
+        }
+
+        private void onBSliderChange(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            byte BsliderValue = (byte)this.Bslider.Value;
+            RGBBackgroundColorChange();
+            this.Blabel.Content = BsliderValue.ToString();
+        }
+
+        private void PobierzClick(object sender, RoutedEventArgs e)
+        {
+            byte RsliderValue = (byte)this.Rslider.Value;
+            byte GsliderValue = (byte)this.Gslider.Value;
+            byte BsliderValue = (byte)this.Bslider.Value;
+            this.Pobrane.Content = RsliderValue.ToString() + "," + GsliderValue.ToString() + "," + BsliderValue.ToString(); 
+            Pobrane.Background = new SolidColorBrush(Color.FromRgb(RsliderValue, GsliderValue, BsliderValue));
         }
     }
 }
